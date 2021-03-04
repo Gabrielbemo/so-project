a60f28c499b711dfb975b07f39bc1726f5d9a914
raqueltsato <raquelsato1@gmail.com>
2021-03-04 15:52:49 -0300

    Add new entities

---
+++ src/main/java/com/lucaszamboni/so/project/Vehicle.java
@@ -0,0 +1,11 @@
+package com.lucaszamboni.so.project;
+
+public class Vehicle {
+    private String licensePlate;
+    private String vehicleName;
+    private String vehicleModel;
+    private Double odometerVehicle;
+    private String vehicleObservation;
+    private boolean waterLevelIsGood;
+    private boolean oilLevelIsGood;
+}
