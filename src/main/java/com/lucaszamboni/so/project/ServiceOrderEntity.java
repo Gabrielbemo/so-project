37742ca0441fe3e8b2141d47f87a4f99f07580d5
Gabrielbemo <gabrielbemoura@gmail.com>
2021-03-03 22:58:16 -0300

    Adicionando os campos do dominio de OS

--- src/main/java/com/lucaszamboni/so/project/ServiceOrderEntity.java
+++ src/main/java/com/lucaszamboni/so/project/ServiceOrderEntity.java
@@ -1,9 +1,25 @@
 package com.lucaszamboni.so.project;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ServiceOrderEntity {
     private String licensePlate;
     private String serviceOrderNumber;
     private LocalDateTime createdData;
+    private String serviceDescription;
+    private String vehicle;
+    private String vehicleModel;
+    private String requestingDriver;
+    private String requestingMaintenance;
+    private LocalDateTime entryVehicleDate;
+    private LocalDateTime requestingAutoPartsDate;
+    private Double odometerVehicle;
+    private String vehicleObservation;
+    private boolean waterLevelIsGood;
+    private boolean oilLevelIsGood;
+    private List<ServiceOrderItem> serviceOrderItems;
+    private LocalDateTime TotalExpectedSpentHours;
+    private LocalDateTime waitApproveDate;
+    private LocalDateTime approveDate;
 }
---
+++ src/main/java/com/lucaszamboni/so/project/ServiceOrderItem.java
@@ -0,0 +1,9 @@
+package com.lucaszamboni.so.project;
+
+import java.time.LocalDateTime;
+
+public class ServiceOrderItem {
+    private String itemNumber;
+    private String itemDescription;
+    private LocalDateTime expectedSpentHours;
+}
